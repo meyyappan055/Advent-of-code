03e38d344ba31c123a6fc4802656ee78bcd0c2c3
meyyappan055 <meyyappan055@gmail.com>
2024-12-04 21:37:19 +0530

    DAY 3 Q2

--- DAY3/D3.py
+++ DAY3/D3.py
@@ -17,3 +17,32 @@ def D3Q1(filename):
     print(total)
 
 D3Q1("D3.txt")
+
+
+
+
+def D3Q2(filename):
+    data = []
+    mul_enabled = True  #Yes Initially 
+
+    with open(filename) as file:
+        for line in file:
+            mul_regex = re.findall(r"mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)", line)
+            for i in mul_regex:
+                if i == "do()":
+                    mul_enabled = True  
+                elif i == "don't()":
+                    mul_enabled = False  
+                elif mul_enabled and i.startswith("mul"):
+                    numbers_in_regex = re.findall(r'\d+', i)
+                    number1, number2 = map(int, numbers_in_regex)
+                    data.append(number1 * number2)
+
+    total = 0
+    for each_value in data:
+        total+=each_value
+
+    return total
+
+
+print(D3Q2("D3.txt"))
